7570b9dcf3bf7ab2bc08308ba3f6ae89e1c210d7
usersblock <thanhphuocly@hotmail.com>
2019-11-14 02:37:17 -0500

    Add files via upload

---
+++ insertcustomerrent.php
@@ -0,0 +1,29 @@
+<?php
+//Connecting
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//Connecting done
+$namevalue=$_POST["name"];
+$ssnvalue=$_POST["ssn"];
+$telephonevalue=$_POST["telephone"];
+$titlevalue=$_POST["title"];
+$borrowdatevalue=$_POST["borrowdate"];
+$durationvalue=$_POST["duration"];
+$sql = "INSERT INTO CUSTOMER(socialsecuritynumber,name,telephone)
+VALUES ('$ssnvalue', '$namevalue','$telephonevalue')";
+$sqltwo = "INSERT INTO RENTS(socialsecuritynumber,title,date,rentperiod)
+VALUES ('$ssnvalue', '$titlevalue','$borrowdatevalue','$durationvalue')";
+
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+if(!mysqli_query($conn,$sqltwo))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
