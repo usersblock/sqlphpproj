b0f6e96fd4b6e2e1a189b95f8727b948c2a566ba
usersblock <thanhphuocly@hotmail.com>
2019-11-14 02:36:16 -0500

    Update testphp.php

--- testphp.php
+++ testphp.php
@@ -51,16 +51,66 @@
   Year: <input type="text" name="year"><br>
 </form>
 <button type="submit" form="findproducer" value="Submit">Submit</button>
+
+CD<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM CD";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Title: " . $row["title"]. " - Year: " . $row["year"]." - Type: " . $row["type"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Song<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM SONG";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"]. " - Artist: " . $row["tracknumber"]. " - Tracknumber: " . $row["tracknumber"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
 Producer<br>
 <?php
 $conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-//Connecting done
-
 $sql = "SELECT * FROM PRODUCER";
-
 if(!mysqli_query($conn,$sql))
 {
  echo("Error description: " . mysqli_error($conn));
@@ -79,6 +129,189 @@ if ($result->num_rows > 0) {
 mysqli_close($conn);
 echo "Connected successfully";
 ?>
+
+Supplier<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM SUPPLIER";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"]. " - Address: " . $row["address"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Customer<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM CUSTOMER";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Ssn: " . $row["socialsecuritynumber"]. " - Name: " . $row["name"]." - Telephone: " . $row["telephone"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Vip<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM CUSTOMER";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Ssn: " . $row["socialsecuritynumber"]. " - Starting date: " . $row["startingdate"]." - Discount: " . $row["discount"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Has<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM HAS";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "title: " . $row["title"]. " - tracknumber: " . $row["tracknumber"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Produces<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM PRODUCES";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "name: " . $row["name"]. " - title: " . $row["title"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Supplied<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM SUPPLIED";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "name: " . $row["name"]. " - title: " . $row["title"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
+Produces<br>
+<?php
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM RENTS";
+if(!mysqli_query($conn,$sql))
+{
+ echo("Error description: " . mysqli_error($conn));
+}
+else{
+$result=mysqli_query($conn,$sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "socialsecuritynumber: " . $row["socialsecuritynumber"]. " - title: " . $row["title"]. " - date: " . $row["date"]. " - rentperiod: " . $row["rentperiod"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+mysqli_close($conn);
+echo "Connected successfully";
+?>
+
 </body>
 </html>
 
