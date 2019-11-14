1446e68c7bd3a9bf3fb0333d58fbb2779021c0d8
usersblock <thanhphuocly@hotmail.com>
2019-11-13 21:40:36 -0500

    Update insertproducer.php

--- insertproducer.php
+++ insertproducer.php
@@ -1,13 +1,13 @@
 <?php
 //Connecting
-$conn = mysqli_connect("134.74.112.21","utest","utest","coursedemo");
+$conn = mysqli_connect("134.74.112.21","ly019f","thomas","d115");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 //Connecting done
 $namevalue=$_POST["name"];
 $addressvalue=$_POST["address"];
-$sql = "INSERT INTO PRODUCER (name,address)
+$sql = "INSERT INTO PRODUCER(name,address)
 VALUES ('$namevalue', '$addressvalue')";
 
 mysql_query($sql);
