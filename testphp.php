925fd2323c718aa3e84a285cb9c770c906ae1780
usersblock <thanhphuocly@hotmail.com>
2019-11-16 18:49:16 -0500

    Update testphp.php

--- testphp.php
+++ testphp.php
@@ -94,7 +94,7 @@ $result=mysqli_query($conn,$sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. " - Artist: " . $row["artist"]. " - Tracknumber: " . $row["tracknumber"]."<br>";
+        echo "Name: " . $row["name"]. " - Artist: " . $row["artist"]. " - Tracknumber: " . $row["tracknumber"]." - Title: " . $row["title"]."<br>";
     }
 } else {
     echo "0 results";
